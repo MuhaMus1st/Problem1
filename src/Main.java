45289149a3841c49a746189d449c06f8f96a3dd1
MuhaMus1st <230375@astanait.edu.kz>
2024-04-07 22:57:30 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+// 1 Problem
+
+public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int n = scanner.nextInt();
+        System.out.println(getMin(n));
+    }
+
+    private static int getMin(int n, int mn) {
+
+        if (n == 0)
+            return mn;
+
+        int num = scanner.nextInt();
+
+        if (mn > num)
+            mn = num;
+
+        return getMin(n - 1, mn);
+    }
+
+    public static int getMin(int n) {
+        return getMin(n, 2147483647);
+    }
+    }
