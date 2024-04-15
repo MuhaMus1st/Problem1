a2851ad004e960766125b73feee8247198c2535c
MuhaMus1st <153479199+MuhaMus1st@users.noreply.github.com>
2024-04-15 08:16:39 +0500

    Pro4.java

--- src/Pro4.java
+++ src/Pro4.java
@@ -1,2 +1,23 @@
+// 4 Pro a
+// Complexity: linear - O(n)
+import java.util.Scanner;
+
 public class Pro4 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        System.out.println(f(n));
+    }
+
+    static int f(int n) {
+
+        if (n == 1)
+            return n;
+
+        return n * f(n - 1);
+    }
 }
