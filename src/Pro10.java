484aaa7b1c8309dd2faa6145e21026e55f592d62
MuhaMus1st <153479199+MuhaMus1st@users.noreply.github.com>
2024-04-15 08:18:26 +0500

    Update Pro10.java

--- src/Pro10.java
+++ src/Pro10.java
@@ -1,2 +1,23 @@
+// 10 Problema uffffffffffffffffffffffffffffffff
+// Logarithmic - O(log(n))
+
+import java.util.Scanner;
+
 public class Pro10 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(gcd(a, b));
+    }
+
+    public static int gcd(int a, int b) {
+
+        if (b == 0)
+            return a;
+
+        return gcd(b, a % b);
+    }
 }
