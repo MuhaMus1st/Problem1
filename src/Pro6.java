99b6b77b3221f4f5d1193417e694abd85295243d
MuhaMus1st <153479199+MuhaMus1st@users.noreply.github.com>
2024-04-15 08:17:23 +0500

    Update Pro6.java

--- src/Pro6.java
+++ src/Pro6.java
@@ -1,2 +1,24 @@
+// 6 Problema
+// linear O(n)
+import java.util.Scanner;
+
 public class Pro6 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int n = scanner.nextInt();
+
+        System.out.println(pow(a, n));
+    }
+
+    public static int pow(int a, int n) {
+
+        if (n == 0) {
+            return 1;
+        }
+
+        return pow(a, n - 1) * a;
+    }
 }
