956949a1548b45243ae60c61041ae5a789d523d6
MuhaMus1st <153479199+MuhaMus1st@users.noreply.github.com>
2024-04-15 08:18:11 +0500

    Update Pro9.java

--- src/Pro9.java
+++ src/Pro9.java
@@ -1,2 +1,20 @@
+// 9 Problemaaaaaaaaaa
+// Exponential - O(2^n)
+import java.util.Scanner;
+
 public class Pro9 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        System.out.println(biCo(n, k));
+    }
+
+    public static int biCo(int n, int k) {
+        if (k == 0 || k == n)
+            return 1;
+
+        return biCo(n - 1, k -1) + biCo(n -1, k);
+    }
 }
