af64b68b316502b7f39854e43c84763859ab7229
MuhaMus1st <153479199+MuhaMus1st@users.noreply.github.com>
2024-04-15 08:17:07 +0500

    Update Pro5.java

--- src/Pro5.java
+++ src/Pro5.java
@@ -1,2 +1,21 @@
+// 5 Problema yep
+// EXPOnential - O(2^n)
+import java.util.Scanner;
+
 public class Pro5 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(fib(n));
+    }
+
+    public static int fib(int n) {
+
+        if (n == 1) return 1;
+        if (n == 0) return 0;
+
+        return fib(n - 1) + fib(n - 2);
+    }
 }
