7c38676fe70beb6f2c00648e9ff31fc63e0911d8
MuhaMus1st <153479199+MuhaMus1st@users.noreply.github.com>
2024-04-15 08:15:45 +0500

    Pro2.java

--- src/Pro2.java
+++ src/Pro2.java
@@ -1,2 +1,31 @@
+// 2 Problem
+// Complexity: linear - O(n)
+import java.util.Scanner;
+
 public class Pro2 {
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int n = scanner.nextInt();
+
+        System.out.println(getAvg(n));
+    }
+
+    public static float getAvg(int n, float sum, int size) {
+        float ans = sum / size ;
+
+        if (n == 0) return ans;
+
+        int a = scanner.nextInt();
+
+        sum += a;
+
+        return getAvg(n - 1, sum, size);
+    }
+
+    private static float getAvg(int n){
+        return getAvg(n, 0, n);
+    }
 }
